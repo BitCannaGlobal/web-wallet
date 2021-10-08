e42165691f28d0ccdc29604c255f6961cffc3f66
ɐʇɯon3ɹ <contact.atmoner@gmail.com>
2021-10-08 21:14:17 +0000

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
+  ],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {
+    'no-console': 0,
+    'vue/comment-directive': 0,
+    'vue/no-mutating-props': 0,
+    'vue/custom-event-name-casing': 0,
+  },
+}
