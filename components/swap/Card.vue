c706ec0ba4fb08d589d9628fecf67611f83336a5
BitCanna Global <info@bitcanna.io>
2021-10-14 12:21:29 +0300

    Update Swap form

--- components/swap/Card.vue
+++ components/swap/Card.vue
@@ -50,6 +50,15 @@
           :msg="requestError"
         />
       </div>
+      <div class="paragraph content">
+        By confirming your address and clicking the "Confirm" button, you
+        expressly warrant to us that you have read our token swap
+        <a
+          href="https://www.bitcanna.io/wp-content/uploads/2021/10/BitCanna-Coin-Swap-TC-13.10.21.pdf"
+          >terms & conditions</a
+        >, that you understand its contents and that you agree with the
+        applicability thereof on the token swap.
+      </div>
       <div class="session-footer first-step">
         <CommonButton value="Confirm" :disabled="loading" />
       </div>
